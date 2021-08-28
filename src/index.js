ad4aaa3fd0d3c4bd1dd8b287229bb1df3effdb2f
VeraEnders <veraabramo@gmail.com>
2021-08-28 22:46:55 +0200

    feat: update js code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = '';
+  
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        brackets += bracketsConfig[i][0];
+        brackets += bracketsConfig[i][1];
+    }
+  
+    let arr = [];
+    for (let a of str) {
+        let index = brackets.indexOf(a);
+        if (index % 2 === 0) {
+            if (brackets[index] === brackets[index + 1] && index + 1 === arr[arr.length - 1]) {
+                arr.pop();
+            } else {
+                arr.push(index + 1);
+            }                      
+        } else {
+            if (arr.pop() !== index) {
+                return false;
+            }
+        }
+    }
+    return arr.length === 0;
 }
